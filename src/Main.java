7424118845d7d08458cd419027c6829aad9197dc
Tristan de Alwis <Tristan_D@Outlook.com>
2025-01-29 15:51:37 -0600

    Added Template for Section 7: OOP Part 1 - Inheritance

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
 //        Section3FirstSteps.main();
 //        Section4IntelliJBasics.main();
 //        Section5ExpressionsStatementsMore.main();
-        Section6ControlFlow.main();
+//        Section6ControlFlow.main();
+        Section7OOPPart1Inheritance.main();
     }
 }
---
+++ src/Section7OOPPart1Inheritance.java
@@ -0,0 +1,127 @@
+package src;
+
+public class Section7OOPPart1Inheritance {
+    public static void main() {
+        System.out.println("Section 6: Control Flow");
+
+        lesson77();
+//        lesson78();
+//        lesson79();
+//        lesson80();
+//        lesson81();
+//        lesson82();
+//        lesson83();
+//        lesson84();
+//        lesson85();
+//        lesson86();
+//        lesson87();
+//        lesson88();
+//        lesson89();
+//        lesson90();
+//        lesson91();
+//        lesson92();
+//        lesson100();
+    }
+
+    private static void lesson77() {
+        System.out.println("Lesson xx: XX\n");
+
+        System.out.println();
+    }
+
+    private static void lesson78() {
+        System.out.println("Lesson xx: XX\n");
+
+        System.out.println();
+    }
+
+    private static void lesson79() {
+        System.out.println("Lesson xx: XX\n");
+
+        System.out.println();
+    }
+
+    private static void lesson80() {
+        System.out.println("Lesson xx: XX\n");
+
+        System.out.println();
+    }
+
+    private static void lesson81() {
+        System.out.println("Lesson xx: XX\n");
+
+        System.out.println();
+    }
+
+    private static void lesson82() {
+        System.out.println("Lesson xx: XX\n");
+
+        System.out.println();
+    }
+
+    private static void lesson83() {
+        System.out.println("Lesson xx: XX\n");
+
+        System.out.println();
+    }
+
+    private static void lesson84() {
+        System.out.println("Lesson xx: XX\n");
+
+        System.out.println();
+    }
+
+    private static void lesson85() {
+        System.out.println("Lesson xx: XX\n");
+
+        System.out.println();
+    }
+
+    private static void lesson86() {
+        System.out.println("Lesson xx: XX\n");
+
+        System.out.println();
+    }
+
+    private static void lesson87() {
+        System.out.println("Lesson xx: XX\n");
+
+        System.out.println();
+    }
+
+    private static void lesson88() {
+        System.out.println("Lesson xx: XX\n");
+
+        System.out.println();
+    }
+
+    private static void lesson89() {
+        System.out.println("Lesson xx: XX\n");
+
+        System.out.println();
+    }
+
+    private static void lesson90() {
+        System.out.println("Lesson xx: XX\n");
+
+        System.out.println();
+    }
+
+    private static void lesson91() {
+        System.out.println("Lesson xx: XX\n");
+
+        System.out.println();
+    }
+
+    private static void lesson92() {
+        System.out.println("Lesson xx: XX\n");
+
+        System.out.println();
+    }
+
+    private static void lesson100() {
+        System.out.println("Lesson xx: XX\n");
+
+        System.out.println();
+    }
+}
