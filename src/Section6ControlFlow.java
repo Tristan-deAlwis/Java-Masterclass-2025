a49638517e8a5dcfd8a25fb47894f5edd5c583d2
Tristan de Alwis <Tristan_D@Outlook.com>
2025-01-28 18:37:15 -0600

    Added structure for Section 6 lessons and Lesson 57

--- src/Section6ControlFlow.java
+++ src/Section6ControlFlow.java
@@ -4,11 +4,157 @@ public class Section6ControlFlow {
     public static void main() {
         System.out.println("Section 6: Control Flow");
 
-//        lesson56();
-//        lesson57();
+        lesson57();
 //        lesson58();
 //        lesson59();
 //        lesson60();
 //        lesson61();
+//        lesson62();
+//        lesson63();
+//        lesson64();
+//        lesson65();
+//        lesson66();
+//        lesson67();
+//        lesson68();
+//        lesson69();
+//        lesson70();
+//        lesson71();
+//        lesson72();
+//        lesson73();
+//        lesson74();
+//        lesson75();
+
+    }
+
+    private static void lesson57() {
+        System.out.println("Lesson 57: The switch statement\n");
+
+        int switchValue = 3;
+
+        switch (switchValue) {
+            case 1:
+                System.out.println("Value was 1");
+                break;
+            case 2:
+                System.out.println("Value was 2");
+                break;
+            case 3: case 4: case 5:
+                System.out.println("Was a 3, a 4, or a 5");
+                System.out.println("Actually it was a " + switchValue);
+                break;
+            default:
+                System.out.println("Was not 1, 2, 3, 4, 5");
+                break;
+        }
+
+        System.out.println();
+    }
+
+    private static void lesson58() {
+        System.out.println("Lesson xx: XX\n");
+
+        System.out.println();
+    }
+
+    private static void lesson59() {
+        System.out.println("Lesson xx: XX\n");
+
+        System.out.println();
+    }
+
+    private static void lesson60() {
+        System.out.println("Lesson xx: XX\n");
+
+        System.out.println();
+    }
+
+    private static void lesson61() {
+        System.out.println("Lesson xx: XX\n");
+
+        System.out.println();
+    }
+
+    private static void lesson62() {
+        System.out.println("Lesson xx: XX\n");
+
+        System.out.println();
+    }
+
+    private static void lesson63() {
+        System.out.println("Lesson xx: XX\n");
+
+        System.out.println();
+    }
+
+    private static void lesson64() {
+        System.out.println("Lesson xx: XX\n");
+
+        System.out.println();
+    }
+
+    private static void lesson65() {
+        System.out.println("Lesson xx: XX\n");
+
+        System.out.println();
+    }
+
+    private static void lesson66() {
+        System.out.println("Lesson xx: XX\n");
+
+        System.out.println();
+    }
+
+    private static void lesson67() {
+        System.out.println("Lesson xx: XX\n");
+
+        System.out.println();
+    }
+
+    private static void lesson68() {
+        System.out.println("Lesson xx: XX\n");
+
+        System.out.println();
+    }
+
+    private static void lesson69() {
+        System.out.println("Lesson xx: XX\n");
+
+        System.out.println();
+    }
+
+    private static void lesson70() {
+        System.out.println("Lesson xx: XX\n");
+
+        System.out.println();
+    }
+
+    private static void lesson71() {
+        System.out.println("Lesson xx: XX\n");
+
+        System.out.println();
+    }
+
+    private static void lesson72() {
+        System.out.println("Lesson xx: XX\n");
+
+        System.out.println();
+    }
+
+    private static void lesson73() {
+        System.out.println("Lesson xx: XX\n");
+
+        System.out.println();
+    }
+
+    private static void lesson74() {
+        System.out.println("Lesson xx: XX\n");
+
+        System.out.println();
+    }
+
+    private static void lesson75() {
+        System.out.println("Lesson xx: XX\n");
+
+        System.out.println();
     }
 }
