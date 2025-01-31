ba31455fa8ad53d9cc8d503d9a533c81067fcf25
Tristan de Alwis <Tristan_D@Outlook.com>
2025-01-30 20:22:18 -0600

    Completed lesson 97

--- src/Section7OOPPart1Inheritance.java
+++ src/Section7OOPPart1Inheritance.java
@@ -14,7 +14,7 @@ public class Section7OOPPart1Inheritance {
 //        lesson92();
 //        lesson93();
 //        lesson94();
-//        lesson97();
+        lesson97();
 //        lesson98();
 //        lesson99();
 //        lesson100();
@@ -559,7 +559,39 @@ public class Section7OOPPart1Inheritance {
     }
 
     private static void lesson97() {
-        System.out.println("Lesson xx: XX\n");
+        System.out.println("Lesson 97: The Text Block and other Formatting Options\n");
+
+        String bulletIt = "Print a Bulleted List:\n" +
+                "\t\u2022 First Point\n" +
+                "\t\t\u2022 Sub Point";
+
+        System.out.println(bulletIt);
+
+        String textBlock = """
+                Print a Bulleted List:
+                    \u2022 First Point
+                        \u2022 SubPoint""";
+
+        System.out.println(textBlock);
+
+        int age = 35;
+        System.out.printf("Your age is %d%n", age);
+
+        int yearOfBirth = 2023 - age;
+        System.out.printf("Age = %d, Birth year = %d%n", age, yearOfBirth);
+
+        System.out.printf("Your age is %.2f%n", (float) age);
+
+        for (int i = 1; i <= 10_000; i *= 10) {
+            System.out.printf("Printing %6d %n", i);
+            }
+
+        String formattedString = String.format("Your age is %d", age);
+        System.out.println(formattedString);
+
+        formattedString = "Your age is %d".formatted(age);
+        System.out.println(formattedString);
+
         System.out.println();
     }
 
