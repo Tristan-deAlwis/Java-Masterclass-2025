81034e40276f102c1a661f8e7cc4bcade2e0c068
Tristan de Alwis <Tristan_D@Outlook.com>
2025-01-30 20:37:28 -0600

    Completed lesson 98

--- src/Section7OOPPart1Inheritance.java
+++ src/Section7OOPPart1Inheritance.java
@@ -14,8 +14,8 @@ public class Section7OOPPart1Inheritance {
 //        lesson92();
 //        lesson93();
 //        lesson94();
-        lesson97();
-//        lesson98();
+//        lesson97();
+        lesson98();
 //        lesson99();
 //        lesson100();
     }
@@ -596,8 +596,63 @@ public class Section7OOPPart1Inheritance {
     }
 
     private static void lesson98() {
-        System.out.println("Lesson xx: XX\n");
+        System.out.println("Lesson 98: Another Look at the String\n");
+
+        printInformation("Hello World");
+        printInformation("");
+        printInformation("\t    \n");
+
+        String helloWorld = "Hello World";
+        System.out.printf("index of r = %d %n", helloWorld.indexOf('r'));
+        System.out.printf("index of World = %d %n", helloWorld.indexOf("World"));
+
+        System.out.printf("index of l = %d %n", helloWorld.indexOf('l'));
+        System.out.printf("index of l = %d %n", helloWorld.lastIndexOf('l'));
+
+        System.out.printf("index of l = %d %n", helloWorld.indexOf('l', 3));
+        System.out.printf("index of l = %d %n", helloWorld.lastIndexOf('l', 8));
+
+        String helloWorldLower = helloWorld.toLowerCase();
+        if (helloWorld.equals(helloWorldLower)) {
+            System.out.println("Values match exactly");
+        }
+        if (helloWorld.equalsIgnoreCase(helloWorldLower)) {
+            System.out.println("Values match ignoring case");
+        }
+
+        if (helloWorld.startsWith("Hello")) {
+            System.out.println("String starts with Hello");
+        }
+        if (helloWorld.endsWith("World")) {
+            System.out.println("String ends with World");
+        }
+        if (helloWorld.contains("World")) {
+            System.out.println("String contains World");
+        }
+        if (helloWorld.contentEquals("Hello World")) {
+            System.out.println("Values match exactly");
+        }
+
         System.out.println();
+    }
+
+    public static void printInformation(String string){
+        int length = string.length();
+        System.out.printf("Length = %d %n", length);
+
+        if (string.isEmpty()) {
+            System.out.println("String is Empty");
+            return;
+        }
+
+        if (string.isBlank()) {
+            System.out.println("String is Blank");
+        }
+        System.out.printf("First char = %c %n", string.charAt(0));
+
+        System.out.printf("Last char = %c %n", string.charAt(length - 1));
+
+
     }
 
     private static void lesson99() {
