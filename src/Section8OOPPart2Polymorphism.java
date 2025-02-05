ebcab0bc00c4409bde596ac44009c41fb471c4d4
Tristan de Alwis <Tristan_D@Outlook.com>
2025-02-05 16:16:13 -0600

    Completed lesson 107

--- src/Section8OOPPart2Polymorphism.java
+++ src/Section8OOPPart2Polymorphism.java
@@ -7,8 +7,8 @@ public class Section8OOPPart2Polymorphism {
 //        lesson103();
 //        lesson104();
 //        lesson105();
-        lesson106();
-//        lesson107();
+//        lesson106();
+        lesson107();
 //        lesson108();
 //        lesson109();
 //        lesson110();
@@ -342,9 +342,55 @@ public class Section8OOPPart2Polymorphism {
         }
     }
 
-        private static void lesson107() {
-        System.out.println("Lesson xxx: XXX\n");
+    private static void lesson107() {
+        System.out.println("Lesson 107: Encapsulation Challenge\n");
+
+        Printer printer = new Printer(50, false);
+        System.out.println("initial page count = " + printer.getPagesPrinted());
+
+        int pagesPrinted = printer.printPages(5);
+        System.out.printf("Current Job Pages: %d, Printer Total: %d %n",
+                pagesPrinted, printer.getPagesPrinted());
+
+        pagesPrinted = printer.printPages(10);
+        System.out.printf("Current Job Pages: %d, Printer Total: %d %n",
+                pagesPrinted, printer.getPagesPrinted());
+
         System.out.println();
+    }
+
+    public static class Printer {
+        private int tonerLevel;
+        private int pagesPrinted;
+        private boolean duplex;
+
+        public Printer(int tonerLevel, boolean duplex) {
+            this.pagesPrinted = 0;
+            this.tonerLevel = (tonerLevel >= 0 && tonerLevel <= 100) ? tonerLevel : -1;
+            this.duplex = duplex;
+        }
+
+        public int addToner(int tonerAmount) {
+
+            int tempAmount = tonerAmount + tonerLevel;
+            if (tempAmount > 100 || tempAmount < 0) {
+                return -1;
+            }
+            tonerLevel += tonerAmount;
+            return tonerLevel;
+        }
+
+        public int printPages(int pages) {
+
+            int jobPages = (duplex) ? (pages / 2) + (pages % 2) : pages;
+            pagesPrinted += jobPages;
+            return jobPages;
+        }
+
+        public int getPagesPrinted() {
+            return pagesPrinted;
+        }
+
     }
 
     private static void lesson108() {
