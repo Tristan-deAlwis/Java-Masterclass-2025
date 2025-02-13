f8eeec7e69f0d8fd683f64beff8f6f81a25d175c
Tristan de Alwis <Tristan_D@Outlook.com>
2025-02-12 21:47:29 -0600

    Completed Lesson 119

--- src/Section9Arrays.java
+++ src/Section9Arrays.java
@@ -6,22 +6,44 @@ public class Section9Arrays {
         System.out.println("Section 9: Arrays");
 
         lesson119();
-        lesson120();
-        lesson121();
-        lesson122();
-        lesson123();
-        lesson124();
-        lesson125();
-        lesson126();
-        lesson127();
-        lesson128();
-        lesson129();
-        lesson130();
+//        lesson120();
+//        lesson121();
+//        lesson122();
+//        lesson123();
+//        lesson124();
+//        lesson125();
+//        lesson126();
+//        lesson127();
+//        lesson128();
+//        lesson129();
+//        lesson130();
     }
 
     private static void lesson119() {
-        System.out.println("Lesson xxx: XXX\n");
+        System.out.println("Lesson 119: Arrays Part 1\n");
 
+        int[] myIntArray = new int[10];
+        myIntArray[0] = 45;
+        myIntArray[1] = 1;
+        myIntArray[5] = 50;
+
+        double[] myDoubleArray = new double[10];
+        myDoubleArray[2] = 3.5;
+        System.out.println(myDoubleArray[2]);
+        System.out.println();
+
+        int[] firstTen = {1,2,3,4,5,6,7,8,9,10};
+        System.out.println("first = " + firstTen[0]);
+        int arrayLength = firstTen.length;
+        System.out.println("length of array = " + arrayLength);
+        System.out.println("last = " + firstTen[arrayLength - 1]);
+        System.out.println();
+
+        int[] newArray;
+        newArray = new int[]  {5,4,3,2,1};
+        for (int i = 0; i < newArray.length; i++) {
+            System.out.println(newArray[i] + " ");
+        }
 
         System.out.println();
     }
@@ -102,3 +124,4 @@ public class Section9Arrays {
 
         System.out.println();
     }
+}
