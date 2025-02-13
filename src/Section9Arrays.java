dd3a50f8686aea714b3353d6949c137e725bf702
Tristan de Alwis <Tristan_D@Outlook.com>
2025-02-12 18:19:22 -0600

    Completed Section 9 Template

--- src/Section9Arrays.java
+++ src/Section9Arrays.java
@@ -5,6 +5,100 @@ public class Section9Arrays {
     public static void main() {
         System.out.println("Section 9: Arrays");
 
-//        lesson103();
-//        lesson104();
+        lesson119();
+        lesson120();
+        lesson121();
+        lesson122();
+        lesson123();
+        lesson124();
+        lesson125();
+        lesson126();
+        lesson127();
+        lesson128();
+        lesson129();
+        lesson130();
+    }
+
+    private static void lesson119() {
+        System.out.println("Lesson xxx: XXX\n");
+
+
+        System.out.println();
+    }
+
+    private static void lesson120() {
+        System.out.println("Lesson xxx: XXX\n");
+
+
+        System.out.println();
+    }
+
+    private static void lesson121() {
+        System.out.println("Lesson xxx: XXX\n");
+
+
+        System.out.println();
+    }
+
+    private static void lesson122() {
+        System.out.println("Lesson xxx: XXX\n");
+
+
+        System.out.println();
+    }
+
+    private static void lesson123() {
+        System.out.println("Lesson xxx: XXX\n");
+
+
+        System.out.println();
+    }
+
+    private static void lesson124() {
+        System.out.println("Lesson xxx: XXX\n");
+
+
+        System.out.println();
+    }
+
+    private static void lesson125() {
+        System.out.println("Lesson xxx: XXX\n");
+
+
+        System.out.println();
+    }
+
+    private static void lesson126() {
+        System.out.println("Lesson xxx: XXX\n");
+
+
+        System.out.println();
+    }
+
+    private static void lesson127() {
+        System.out.println("Lesson xxx: XXX\n");
+
+
+        System.out.println();
+    }
+
+    private static void lesson128() {
+        System.out.println("Lesson xxx: XXX\n");
+
+
+        System.out.println();
+    }
+
+    private static void lesson129() {
+        System.out.println("Lesson xxx: XXX\n");
+
+
+        System.out.println();
+    }
+
+    private static void lesson130() {
+        System.out.println("Lesson xxx: XXX\n");
+
+
+        System.out.println();
     }
