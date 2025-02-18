100e1ee6cc6bb5d64bee209e567b5b03e8579812
Tristan de Alwis <Tristan_D@Outlook.com>
2025-02-18 17:17:04 -0600

    Completed lesson 134

--- src/Section10ListArrayListLinkedListIteratorAutoboxing.java
+++ src/Section10ListArrayListLinkedListIteratorAutoboxing.java
@@ -2,6 +2,7 @@ package src;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class Section10ListArrayListLinkedListIteratorAutoboxing {
@@ -9,8 +10,8 @@ public class Section10ListArrayListLinkedListIteratorAutoboxing {
         System.out.println("Section 9: Arrays");
 
 //        lesson132();
-        lesson133();
-//        lesson134();
+//        lesson133();
+        lesson134();
 //        lesson135();
 //        lesson136();
 //        lesson137();
@@ -88,7 +89,65 @@ public class Section10ListArrayListLinkedListIteratorAutoboxing {
     }
 
     private static void lesson134() {
-        System.out.println("Lesson xx: XXX\n");
+        System.out.println("Lesson 134: ArrayList Part 3\n");
+
+        String[] items = {"apples", "bananas", "milk", "eggs"};
+
+        List<String> list = List.of(items);
+        System.out.println(list);
+
+        ArrayList<String> groceries = new ArrayList<>(list);
+        groceries.add("yogurt");
+        System.out.println(groceries);
+
+        ArrayList<String> nextList = new ArrayList<>(
+                List.of("pickles", "mustard", "cheese"));
+        System.out.println(nextList);
+
+        groceries.addAll(nextList);
+        System.out.println(groceries);
+
+        System.out.println("Third item = " + groceries.get(2));
+
+        if (groceries.contains("mustard")) {
+            System.out.println("List contains mustard");
+        }
+
+        groceries.add("yogurt");
+        System.out.println("first = " + groceries.indexOf("yogurt"));
+        System.out.println("last = " + groceries.lastIndexOf("yogurt"));
+
+        System.out.println(groceries);
+        groceries.remove(1);
+        System.out.println(groceries);
+        groceries.remove("yogurt");
+        System.out.println(groceries);
+
+        groceries.removeAll(List.of("apples", "eggs"));
+        System.out.println(groceries);
+
+        groceries.retainAll(List.of("apples", "milk", "mustard",
+                "cheese"));
+        System.out.println(groceries);
+
+        groceries.clear();
+        System.out.println(groceries);
+        System.out.println("isEmpty = " + groceries.isEmpty());
+
+        groceries.addAll(List.of("apples", "milk", "mustard",
+                "cheese"));
+        groceries.addAll(Arrays.asList("eggs", "pickles", "mustard", "ham"));
+
+        System.out.println(groceries);
+        groceries.sort(Comparator.naturalOrder());
+        System.out.println(groceries);
+
+        groceries.sort(Comparator.reverseOrder());
+        System.out.println(groceries);
+
+        var groceryArray = groceries.toArray(new String[groceries.size()]);
+        System.out.println(Arrays.toString(groceryArray));
+
         System.out.println();
     }
 
