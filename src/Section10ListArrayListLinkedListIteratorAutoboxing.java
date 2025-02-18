db152062eacc0998af47e350a913b55f1af4b59b
Tristan de Alwis <Tristan_D@Outlook.com>
2025-02-18 16:29:41 -0600

    Completed lesson 133

--- src/Section10ListArrayListLinkedListIteratorAutoboxing.java
+++ src/Section10ListArrayListLinkedListIteratorAutoboxing.java
@@ -2,13 +2,14 @@ package src;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Section10ListArrayListLinkedListIteratorAutoboxing {
     public static void main(String[] args) {
         System.out.println("Section 9: Arrays");
 
-        lesson132();
-//        lesson133();
+//        lesson132();
+        lesson133();
 //        lesson134();
 //        lesson135();
 //        lesson136();
@@ -49,10 +50,40 @@ public class Section10ListArrayListLinkedListIteratorAutoboxing {
         public GroceryItem(String name) {
             this(name, "DAIRY", 1);
         }
+
+        @Override
+        public String toString() {
+            return String.format("%d %s in %s", count, name.toUpperCase(), type);
+        }
     }
 
     private static void lesson133() {
-        System.out.println("Lesson xx: XXX\n");
+        System.out.println("Lesson 133: ArrayList Part 2\n");
+        ArrayList<GroceryItem> groceryList = new ArrayList<>();
+        groceryList.add(new GroceryItem("Butter"));
+        groceryList.add(new GroceryItem("milk"));
+        groceryList.add(new GroceryItem("oranges", "PRODUCE", 5));
+        groceryList.set(0,
+                new GroceryItem("apples", "PRODUCE", 6));
+        groceryList.remove(1);
+        System.out.println(groceryList);
+
+        String[] items = {"apples", "bananas", "milk", "eggs"};
+
+        List<String> list = List.of(items);
+        System.out.println(list);
+
+        ArrayList<String> groceries = new ArrayList<>(list);
+        groceries.add("yogurt");
+        System.out.println(groceries);
+
+        ArrayList<String> nextList = new ArrayList<>(
+                List.of("pickles", "mustard", "cheese"));
+        System.out.println(nextList);
+
+        groceries.addAll(nextList);
+        System.out.println(groceries);
+
         System.out.println();
     }
 
