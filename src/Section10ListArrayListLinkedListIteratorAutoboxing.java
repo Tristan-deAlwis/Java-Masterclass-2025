1a23482c7d7e671ad3f0b228a3b770ace904d18a
Tristan-DeAlwis <Tristan_D@outlook.com>
2025-02-18 20:53:10 -0600

    Completed lesson 138

--- src/Section10ListArrayListLinkedListIteratorAutoboxing.java
+++ src/Section10ListArrayListLinkedListIteratorAutoboxing.java
@@ -11,7 +11,7 @@ public class Section10ListArrayListLinkedListIteratorAutoboxing {
 //        lesson134();
 //        lesson135();
 //        lesson136();
-//        lesson138();
+        lesson138();
 //        lesson139();
 //        lesson140();
 //        lesson141();
@@ -210,8 +210,63 @@ public class Section10ListArrayListLinkedListIteratorAutoboxing {
     }
 
     private static void lesson138() {
-        System.out.println("Lesson xx: XXX\n");
+        System.out.println("Lesson 138: LinkedList Part 1\n");
+
+//        LinkedList<String> placesToVisit = new LinkedList<>();
+        var placesToVisit = new LinkedList<String>();
+
+        placesToVisit.add("Sydney");
+        placesToVisit.add(0, "Canberra");
+        System.out.println(placesToVisit);
+
+        addMoreElements(placesToVisit);
+        System.out.println(placesToVisit);
+
+        removeElements(placesToVisit);
+        System.out.println(placesToVisit);
+
         System.out.println();
+    }
+
+    public static void addMoreElements(LinkedList<String> list) {
+        list.addFirst("Darwin");
+        list.addFirst("Hobart");
+//        Queue methods
+        list.offer("Melbourne");
+        list.offerFirst("Brisbane");
+        list.offerLast("Toowoomba");
+//        Stack Methods
+        list.push("Alice Springs");
+    }
+
+    public static void removeElements(LinkedList<String> list) {
+        list.remove(4);
+        list.remove("Brisbane");
+
+        System.out.println(list);
+        String s1 = list.remove(); // removes first element
+        System.out.println(s1 + " was removed");
+
+        String s2 = list.removeFirst(); // removes first element
+        System.out.println(s2 + " was removed");
+
+        String s3 = list.removeLast(); // removes last element
+        System.out.println(s3 + " was removed");
+//        Queue/Deque poll methods
+        String p1 = list.poll(); // removes first element
+        System.out.println(p1 + " was removed");
+        String p2 = list.pollFirst(); // removes first element
+        System.out.println(p2 + " was removed");
+        String p3 = list.pollLast(); // removes last element
+        System.out.println(p3 + " was removed");
+
+        list.push("Sydney");
+        list.push("Brisbane");
+        list.push("Canberra");
+        System.out.println(list);
+
+        String p4 = list.pop();
+        System.out.println(p4 + " was removed");
     }
 
     private static void lesson139() {
