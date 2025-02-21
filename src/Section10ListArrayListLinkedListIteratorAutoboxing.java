e81365ca2f3d07a0099613d273da17eb095fdfba
Tristan-DeAlwis <Tristan_D@outlook.com>
2025-02-21 14:29:42 -0600

    Completed lesson 146

--- src/Section10ListArrayListLinkedListIteratorAutoboxing.java
+++ src/Section10ListArrayListLinkedListIteratorAutoboxing.java
@@ -18,8 +18,8 @@ public class Section10ListArrayListLinkedListIteratorAutoboxing {
 //        lesson142();
 //        lesson143();
 //        lesson144();
-        lesson145();
-//        lesson146();
+//        lesson145();
+        lesson146();
 //        lesson147();
 //        lesson148();
     }
@@ -598,15 +598,23 @@ public class Section10ListArrayListLinkedListIteratorAutoboxing {
         }
     }
 
-    class Bank {
+    static class Bank {
         private String name;
-        private ArrayList<Customer> customers = new ArrayList<>(5000);
+        private static ArrayList<Customer> customers = new ArrayList<>(5000);
 
         public Bank(String name) {
             this.name = name;
         }
 
-        private Customer getCustomer(String customerName) {
+        @Override
+        public String toString() {
+            return "Bank{" +
+                    "name='" + name + '\'' +
+                    ", customers=" + customers +
+                    '}';
+        }
+
+        private static Customer getCustomer(String customerName) {
             for (var customer : customers) {
                 if (customer.name().equalsIgnoreCase(customerName)) {
                     return customer;
@@ -624,10 +632,45 @@ public class Section10ListArrayListLinkedListIteratorAutoboxing {
                 System.out.println("New Customer added: " + customer);
             }
         }
+
+        public void addTransaction(String name, double transactionAmount) {
+            Customer customer = Bank.getCustomer(name);
+            if (customer != null) {
+                customer.transactions().add(transactionAmount);
+            }
+        }
+
+        public void printStatement(String customerName) {
+            Customer customer = Bank.getCustomer(customerName);
+            if (customer == null) {
+                return;
+            }
+
+            System.out.println("-".repeat(30));
+            System.out.println("Customer Name: " + customer.name());
+            System.out.println("Transaction:");
+            for (double d : customer.transactions()) {
+                System.out.printf("$%10.2f (%s)%n", d, d < 0 ? "debit" : "credit");
+            }
+
+        }
     }
 
     private static void lesson146() {
-        System.out.println("Lesson xx: XXX\n");
+        System.out.println("Lesson 146: Autoboxing & Unboxing Challenge\n");
+
+        Bank bank = new Bank("Chase");
+        bank.addNewCustomer("Jane A", 500.0);
+        System.out.println(bank);
+
+        bank.addTransaction("Jane A", -10.25);
+        bank.addTransaction("Jane A", -75.01);
+        bank.printStatement("Jane a");
+
+        bank.addNewCustomer("Bob S", 25);
+        bank.addTransaction("Bob S", 100);
+        bank.printStatement("Bob S");
+
         System.out.println();
     }
 
