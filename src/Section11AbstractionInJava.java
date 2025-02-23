445ef970aaebbe8b8ae5a789e27b914b6ef6ce7d
Tristan de Alwis <Tristan_D@Outlook.com>
2025-02-22 18:33:44 -0600

    Created Section 11 Outline

---
+++ src/Section11AbstractionInJava.java
@@ -0,0 +1,86 @@
+package src;
+
+public class Section11AbstractionInJava {
+    public static void main(String[] args) {
+        System.out.println("Section 9: Arrays");
+
+        lesson151();
+        lesson152();
+        lesson153();
+        lesson154();
+        lesson155();
+        lesson156();
+        lesson157();
+        lesson158();
+        lesson159();
+        lesson160();
+        lesson161();
+        lesson162();
+        lesson163();
+    }
+
+    private static void lesson151() {
+        System.out.println("Lesson xx: XX\n");
+        System.out.println();
+    }
+
+    private static void lesson152() {
+        System.out.println("Lesson xx: XX\n");
+        System.out.println();
+    }
+
+    private static void lesson153() {
+        System.out.println("Lesson xx: XX\n");
+        System.out.println();
+    }
+
+    private static void lesson154() {
+        System.out.println("Lesson xx: XX\n");
+        System.out.println();
+    }
+
+    private static void lesson155() {
+        System.out.println("Lesson xx: XX\n");
+        System.out.println();
+    }
+
+    private static void lesson156() {
+        System.out.println("Lesson xx: XX\n");
+        System.out.println();
+    }
+
+    private static void lesson157() {
+        System.out.println("Lesson xx: XX\n");
+        System.out.println();
+    }
+
+    private static void lesson158() {
+        System.out.println("Lesson xx: XX\n");
+        System.out.println();
+    }
+
+    private static void lesson159() {
+        System.out.println("Lesson xx: XX\n");
+        System.out.println();
+    }
+
+    private static void lesson160() {
+        System.out.println("Lesson xx: XX\n");
+        System.out.println();
+    }
+
+    private static void lesson161() {
+        System.out.println("Lesson xx: XX\n");
+        System.out.println();
+    }
+
+    private static void lesson162() {
+        System.out.println("Lesson xx: XX\n");
+        System.out.println();
+    }
+
+    private static void lesson163() {
+        System.out.println("Lesson xx: XX\n");
+        System.out.println();
+    }
+}
