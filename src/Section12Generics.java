bf013680f943213eeedc9dd00b061460d3e09888
Tristan de Alwis <Tristan_D@Outlook.com>
2025-02-24 16:16:34 -0600

    Completed Lesson 168

--- src/Section12Generics.java
+++ src/Section12Generics.java
@@ -1,6 +1,7 @@
 package src;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Section12Generics {
@@ -10,7 +11,7 @@ public class Section12Generics {
         lesson165();
         lesson166();
         lesson167();
-//        lesson168();
+        lesson168();
 //        lesson169();
 //        lesson170();
 //        lesson171();
@@ -307,8 +308,63 @@ public class Section12Generics {
     }
 
     private static void lesson168() {
-        System.out.println("Lesson xx: XX\n");
+        System.out.println("Lesson 168: Generic Class Challenge\n");
         System.out.println();
+    }
+
+    public interface Mappable {
+
+        void render();
+
+        static double[] stringToLatLon(String location) {
+
+            var splits = location.split(",");
+            double lat = Double.valueOf(splits[0]);
+            double lng = Double.valueOf(splits[1]);
+            return new double[]{lat, lng};
+        }
+    }
+
+    abstract class Point implements Mappable {
+
+        private double[] location = new double[2];
+
+        public Point(String location) {
+            this.location = Mappable.stringToLatLon(location);
+        }
+
+        @Override
+        public void render() {
+
+            System.out.println("Render " + this + " as POINT (" + location() + ")");
+        }
+
+        private String location() {
+            return Arrays.toString(location);
+        }
+    }
+
+
+    abstract class Line implements Mappable {
+
+        private double[][] locations;
+
+        public Line(String... locations) {
+            this.locations = new double[locations.length][];
+            int index = 0;
+            for (var l : locations) {
+                this.locations[index++] = Mappable.stringToLatLon(l);
+            }
+        }
+
+        @Override
+        public void render() {
+            System.out.println("Render " + this + " as LINE (" + locations() + ")");
+        }
+
+        private String locations() {
+            return Arrays.deepToString(locations);
+        }
     }
 
     private static void lesson169() {
