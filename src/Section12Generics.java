693dd6abf07dbaf4ee95dae7538d654b4db8203f
Tristan de Alwis <Tristan_D@Outlook.com>
2025-02-24 16:24:05 -0600

    Completed Lesson 169

--- src/Section12Generics.java
+++ src/Section12Generics.java
@@ -12,7 +12,7 @@ public class Section12Generics {
         lesson166();
         lesson167();
         lesson168();
-//        lesson169();
+        lesson169();
 //        lesson170();
 //        lesson171();
 //        lesson172();
@@ -325,7 +325,7 @@ public class Section12Generics {
         }
     }
 
-    abstract class Point implements Mappable {
+    abstract static class Point implements Mappable {
 
         private double[] location = new double[2];
 
@@ -345,7 +345,7 @@ public class Section12Generics {
     }
 
 
-    abstract class Line implements Mappable {
+    abstract static class Line implements Mappable {
 
         private double[][] locations;
 
@@ -368,8 +368,85 @@ public class Section12Generics {
     }
 
     private static void lesson169() {
-        System.out.println("Lesson xx: XX\n");
+        System.out.println("Lesson 169: Generic Class Challenge, Part 2\n");
+        var nationalUSParks = new Park[]{
+                new Park("Yellowstone", "44.4882, -110.5916"),
+                new Park("Grand Canyon", "36.1085, -112.0965"),
+                new Park("Yosemite", "37.8855, -119.5360")
+        };
+
+        Layer<Park> parkLayer = new Layer<>(nationalUSParks);
+        parkLayer.renderLayer();
+
+        var majorUSRivers = new River[]{
+                new River("Mississippi",
+                        "47.2160, -95.2348", "29.1566, -89.2495",
+                        "35.1556, -90.0659"),
+                new River("Missouri",
+                        "45.9239, -111.4983", "38.8146, -90.1218")
+        };
+
+        Layer<River> riverLayer = new Layer<>(majorUSRivers);
+
+        riverLayer.addElements(
+                new River("Colorado",
+                        "40.4708, -105.8286", "31.7811, -114.7724"),
+                new River("Delaware",
+                        "42.2026, -75.00836", "39.4955, -75.5592"));
+
+        riverLayer.renderLayer();
+
         System.out.println();
+    }
+
+    public static class Layer <T extends Mappable> {
+
+        private List<T> layerElements;
+
+        public Layer(T[] layerElements) {
+            this.layerElements = new ArrayList<T>(List.of(layerElements));
+        }
+
+        public void addElements(T... elements) {
+            layerElements.addAll(List.of(elements));
+        }
+
+        public void renderLayer() {
+
+            for (T element : layerElements) {
+                element.render();
+            }
+        }
+    }
+
+    public static class River extends Line {
+
+        private String name;
+
+        public River(String name, String... locations) {
+            super(locations);
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return name + " River";
+        }
+    }
+
+    public static class Park extends Point {
+
+        private String name;
+
+        public Park(String name, String location) {
+            super(location);
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return name + " National Park";
+        }
     }
 
     private static void lesson170() {
