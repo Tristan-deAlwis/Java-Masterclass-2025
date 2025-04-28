8b3eccd0af990b3ead4ee0ce26e5551ce43da902
Tristan de Alwis <Tristan_D@Outlook.com>
2025-04-28 16:33:45 -0500

    Completed Lesson 170

--- src/Section12Generics.java
+++ src/Section12Generics.java
@@ -13,7 +13,7 @@ public class Section12Generics {
         lesson167();
         lesson168();
         lesson169();
-//        lesson170();
+        lesson170();
 //        lesson171();
 //        lesson172();
 //        lesson173();
@@ -450,8 +450,61 @@ public class Section12Generics {
     }
 
     private static void lesson170() {
-        System.out.println("Lesson xx: XX\n");
+        System.out.println("Lesson 170: Comparable Revisited: Sorting Objects by Natural Order\n");
+
+        Integer five = 5;
+        Integer[] others = {0, 5, 10, -50, 50};
+
+        for (Integer i : others) {
+            int val = five.compareTo(i);
+            System.out.printf("%d %s %d: compareTo=%d%n", five, (val == 0 ? "==" : (val <0) ? "<" : ">"), i, val);
+        }
+
+        String banana = "banana";
+        String[] fruit = {"apple", "banana", "pear", "BANANA"};
+
+        for (String s : fruit) {
+            int val = banana.compareTo(s);
+            System.out.printf("%s %s %s: compareTo=%d%n", banana, (val == 0 ? "==" : (val <0) ? "<" : ">"), s, val);
+        }
+
+        Arrays.sort(fruit);
+        System.out.println(Arrays.toString(fruit));
+
+        System.out.println("A:"+(int)'A' + " " + "a:"+(int)'a');
+        System.out.println("B:"+(int)'B' + " " + "b:"+(int)'b');
+        System.out.println("P:"+(int)'P' + " " + "p:"+(int)'p');
+
+        Student tim = new Student ("Tim");
+        Student [] students = {new Student("Zach"), new Student("Tim"),
+                new Student("Ann")};
+
+        Arrays.sort(students);
+        System.out.println(Arrays.toString(students));
+
+        System.out.println("result = " + tim.compareTo("Mary"));
+
         System.out.println();
+    }
+
+    private static class Student implements Comparable {
+
+        private String name;
+
+        public Student(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+
+        @Override
+        public int compareTo(Object o) {
+            Student other = (Student) o;
+            return name.compareTo(other.name);
+        }
     }
 
     private static void lesson171() {
